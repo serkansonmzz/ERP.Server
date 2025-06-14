3d347d1737e0b3a7986d94b591fec156f09f1de0
Serkan Sönmez <serkansonmzz@gmail.com>
2025-06-14 20:51:16 +0300

    Add ICongifuration parametre

--- ERP.Server.Infrastructure/DependencyInjection.cs
+++ ERP.Server.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,17 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ERP.Server.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
         // Tüm Infrastructure servisleri burada kaydedilecek
+        // Örnek: var connectionString = configuration.GetConnectionString("DefaultConnection");
+        
         return services;
     }
 }
