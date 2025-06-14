41d6fe700371038bd5de0d6ef9ff7be9aa752d89
Serkan Sönmez <serkansonmzz@gmail.com>
2025-06-14 19:05:37 +0300

    Entitites/Enum direcctory is fixed

--- ERP.Server.Domain/Interfaces/Repositories/IProductRepository.cs
+++ ERP.Server.Domain/Interfaces/Repositories/IProductRepository.cs
@@ -1,10 +1,11 @@
 using ERP.Server.Domain.Entities;
+using ERP.Server.Domain.Entities.Enums;
 
 namespace ERP.Server.Domain.Interfaces.Repositories;
 
 public interface IProductRepository : IRepository<Product>
 {
     // Product'a özel sorgular buraya eklenecek
-    Task<IReadOnlyList<Product>> GetProductsByTypeAsync(Enums.ProductType type, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<Product>> GetProductsByTypeAsync(ProductType type, CancellationToken cancellationToken = default);
     Task<IReadOnlyList<Product>> SearchProductsAsync(string searchTerm, CancellationToken cancellationToken = default);
 }
