91e203e691e680a2909b37e624d4a8f590616049
Serkan Sönmez <serkansonmzz@gmail.com>
2025-06-15 13:49:19 +0300

    add configurations for ServiceRegistiration

--- ERP.Server.Application/ServiceRegistration.cs
+++ ERP.Server.Application/ServiceRegistration.cs
@@ -8,7 +8,7 @@ namespace ERP.Server.Application;
 
 public static class ServiceRegistration
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services, )
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
 
         services.AddLogging(loggingBuilder => 
